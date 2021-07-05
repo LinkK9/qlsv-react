c93e1bb1ff38b755d974a31c5622f7faa6ea2508
Sylk <lekhasy@outlook.com>
2021-07-05 19:49:46 +0700

    add use-promise-result

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
 import { AppHeader } from "../AppHeader/AppHeader";
 import StudentList from "../StudentList/StudentList";
-import mockData from "../../MOCK_DATA";
+import { usePromiseResult } from "use-promise-result";
+import { getAllStudent } from "../../servies/StudentService";
 
 const Home = () => {
-	const [data, setData] = useState(mockData);
+  const { loading, error, success, reload, data } =
+    usePromiseResult(getAllStudent);
 
-	const handleSearch = (inputVal) => {
-		if (inputVal === "") {
-			setData(mockData);
-		}
-		const result = mockData.filter(student => student.name.includes(inputVal));
-		setData(result);
-	}
+  const [searchingValue, setSearchingValue] = useState("");
+
+  const handleSearch = (inputVal) => {
+    setSearchingValue(inputVal);
+
+    // if (inputVal === "") {
+    //   setData(mockData);
+    // }
+    // const result = mockData.filter((student) =>
+    //   student.name.includes(inputVal)
+    // );
+    // setData(result);
+  };
+
+  const filteredList = []; //
 
   return (
     <div>
-      <AppHeader handleSearch={handleSearch}  />
-      <StudentList data={data} />
+      <AppHeader handleSearch={handleSearch} />
+      <StudentList data={filteredList} />
     </div>
   );
 };
