65cfe10f0fe668eda221dc52dff4d407f16509ff
Sylk <lekhasy@outlook.com>
2021-07-07 20:10:50 +0700

    Add student screen and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import 'antd/dist/antd.css';
-import Home from './components/Home/Home';
+import "antd/dist/antd.css";
+import Home from "./components/Home/Home";
+import AddStudent from "./components/AddStudent/AddStudent";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// add-student
 
 function App() {
-  return <div className="App">
-    <Home />
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="add-student">
+            <AddStudent />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
