4e2c99084ff88bfab2ed94b1b126cecb41405922
Linh Trinh Viet <73121552+LinkK9@users.noreply.github.com>
2021-07-12 18:56:48 +0700

    completed edit form

--- src/components/ModifyStudent/ModifyStudent.js
+++ src/components/ModifyStudent/ModifyStudent.js
@@ -1,7 +1,21 @@
 import { useParams } from "react-router-dom";
+import StudentDetailForm from "../StudentDetailForm";
+import { usePromiseResult } from "use-promise-result";
+import { getStudent, modifyStudentAction } from "../../servies/StudentService";
 
 export default function ModifyStudent() {
   let { id } = useParams();
+  const { loading, error, success, reload, data } = usePromiseResult(() =>
+    getStudent(id)
+  );
 
-  return "Student id:" + id;
+  const renderForm = () => {
+    return <StudentDetailForm initialValues={data} action={modifyStudentAction} />;
+  };
+
+  const renderStatus = () => {
+    if (loading) return <div>loading..</div>;
+    if (error) return "Error...";
+  };
+  return <div>{success ? renderForm() : renderStatus()}</div>;
 }
--- src/servies/StudentService.js
+++ src/servies/StudentService.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import { getAvatarUrlFromFileName } from "../utils/Utils";
+import moment from "moment";
 
 const API = "http://localhost:5000/Student";
 const addStudentEndPoint = API + "/AddStudent";
+const getStudentEndPoint = API + "/GetStudent";
+const modifyStudentEndPoint = API + "/ModifyStudent";
 
 const delayAsync = (timeout) => {
   return new Promise((resovle) => {
@@ -58,4 +62,45 @@ export const addStudent = async (newStudent) => {
       "Content-Type": `multipart/form-data boundary=${fd._boundary}`,
     },
   });
+};
+
+export const getStudent = async (id) => {
+  return await axios
+    .get(getStudentEndPoint, {
+      params: {
+        id,
+      },
+    })
+    .then((res) => res.data.data)
+    .then((data) => {
+      return {
+        ...data,
+        img: getAvatarUrlFromFileName(data.img),
+        birthday: moment(data.birthday).format("YYYY-MM-DD"),
+        gender: String(data.gender),
+        dayAdmission: moment(data.dayAdmission).format("YYYY-MM-DD"),
+      };
+    });
+};
+
+export const modifyStudentAction = async (initVal) => {
+  let fd = new FormData();
+
+  initVal = {
+    ...initVal,
+    gender: +initVal.gender,
+  };
+  for(let key in initVal){
+    const value = initVal[key];
+    fd.append(key, value);
+  }
+  return await axios.post(
+    modifyStudentEndPoint,
+    fd,
+    {
+      headers: {
+        "Content-Type": false,
+      },
+    }
+  );
 };
